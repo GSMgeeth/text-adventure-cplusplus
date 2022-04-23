4dcfdc1d891b6e97531cb518c20cce4e9a5403e6
Geeth Sandaru <geeth.gsm@gmail.com>
2022-04-24 05:28:23 +0530

    added a todo list

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -1,3 +1,21 @@
+/**
+ * @file text-adventure.cpp
+ * @author Geeth Sandaru (geeth.gsm@gmail.com)
+ * @brief A game of storytelling where user can select where the story goes.
+ * @version 0.1
+ * @date 2022-04-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * @todo
+ * - Design a story where user can achieve the dream if work hard.
+ * - Add at least 3 different endings.
+ * - Design paths where they go in separate directions but at some point joins a one major path.
+ * - Having fun not always take him down but there's a limit.
+ * - Have some ASCII arts to make the game not boring. https://en.wikipedia.org/wiki/ASCII_art
+ * 
+ */
+
 #include <iostream>
 
 int main() {
