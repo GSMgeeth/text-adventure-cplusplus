4cb1d0363180b57311ce37289091acad2600483c
Geeth Sandaru <geeth.gsm@gmail.com>
2022-04-24 18:11:28 +0530

    added lucid link to flowchart

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2022
  * 
  * @todo
- * - Design a story where user can achieve the dream if work hard.
+ * - Design a story where user can achieve the dream if work hard. (https://lucid.app/lucidchart/ff7836d2-3b5d-4a37-ac48-ea45fd826246/edit?beaconFlowId=ACE9CDEAD7FB2AF4&page=0_0&invitationId=inv_7e5bc962-de02-4ca6-b2d4-036f2b27a0cf#)
  * - Add at least 3 different endings.
  * - Design paths where they go in separate directions but at some point joins a one major path.
  * - Having fun not always take him down but there's a limit.
