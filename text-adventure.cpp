0d92db31b177548194cd78d77b30eb76fa52e1c2
Geeth Sandaru <geeth.gsm@gmail.com>
2022-04-28 01:31:17 +0530

    variable types changed

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -17,15 +17,15 @@
  */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void getAnswer(const string question, int &answer, const int noOfAnswers);
+void getAnswer(const string question, int &answer, const int noOfOptions);
 
 int main() {
     int score = 7;
-    int answer = 0;
-    int previousAnswer = 0;
+    int answers[5] = {0, 0, 0, 0, 0};
 
     cout << "Text Adventure" << endl
          << endl;
@@ -34,60 +34,65 @@ int main() {
          << "From now on, you are Bob and you need to work hard to achieve your goals." << endl
          << "Let's begin!" << endl;
 
-    getAnswer("(1) Work / study or (2) play ? ", answer, 2);
+    getAnswer("(1) Work or (2) play ? ", answers[0], 2);
 
     // First answer evaluation
-    switch (answer) {
+    switch (answers[0]) {
         case 1:
             score++;
-            getAnswer("You worked / learnt. Next, (1) worked / learnt or (2) play ? ", answer, 2);
+            getAnswer("You worked. Next, (1) work or (2) play ? ", answers[1], 2);
             break;
         case 2:
-            getAnswer("You played. Next, (1) worked / learnt or (2) play ? ", answer, 2);
+            getAnswer("You played. Next, (1) work or (2) play ? ", answers[1], 2);
             break;
         default:
             cout << "The answer is incorrect! Please try again." << endl;
-            answer = 0;
             break;
     }
 
     // Second answer evaluation
-    switch (answer) {
+    switch (answers[1]) {
         case 1:
             score++;
-            getAnswer("You worked / learnt. Next, (1) worked / learnt or (2) sleep ? ", answer, 2);
+            getAnswer("You worked. Next, (1) work or (2) sleep ? ", answers[2], 2);
             break;
         case 2:
-            score = previousAnswer == 1 ? score : score - 1;
-            getAnswer("You played. Next, (1) worked / learnt or (2) sleep ? ", answer, 2);
+            score = answers[0] == 1 ? score : score - 1;
+            getAnswer("You played. Next, (1) work or (2) sleep ? ", answers[2], 2);
             break;
         default:
             cout << "The answer is incorrect! Please try again." << endl;
-            answer = 0;
             break;
     }
 
     // Third answer evaluation
-    switch (answer) {
+    switch (answers[2]) {
         case 1:
             score--;
-            getAnswer("You worked / learnt. Next, (1) worked / learnt or (2) sleep ? ", answer, 2);
+            getAnswer("You worked. Next, (1) work or (2) sleep ? ", answers[3], 2);
             break;
         case 2:
             score++;
-            getAnswer("You played. Next, (1) worked / learnt or (2) sleep ? ", answer, 2);
+            getAnswer("You played. Next, (1) work or (2) sleep ? ", answers[3], 2);
             break;
         default:
             cout << "The answer is incorrect! Please try again." << endl;
-            answer = 0;
             break;
     }
 }
 
-void getAnswer(const string question, int &answer, const int noOfAnswers = 3) {
+/**
+ * @brief Get the answer from user an set to answer reference.
+ * getAnswer keeps asking for the answer until the user gives a valid answer.
+ * 
+ * @param question the question text
+ * @param answer the answer reference
+ * @param noOfOptions no of options to choose from
+ */
+void getAnswer(const string question, int &answer, const int noOfOptions = 2) {
     do {
         cout << question << endl;
         cin >> answer;
 
-    } while (answer <= 0 || answer > noOfAnswers);
+    } while (answer <= 0 || answer > noOfOptions);
 }
